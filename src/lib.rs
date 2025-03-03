bbc0d28ce45de0d1ea16567d03d3fbaf4c20c3aa
Meph1sto666 <phentragon@gmx.de>
2025-03-03 14:03:51 +0100

    feat(song): base functions
    song detail, songs list and auto-play fetching

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
+pub mod types;
+
 #[cfg(test)]
 mod tests {
-    
+	use crate::types::song::{fetch_autoplay_song_id, fetch_song_details, fetch_song_list, Song};
+	use tokio;
+
+	#[tokio::test]
+    async fn test_fetch_song_list() {
+		let song_list: Vec<crate::types::song::SongSyn> = fetch_song_list().await.unwrap();
+		assert!(song_list.len() > 0);
+	}
+	#[tokio::test]
+	async fn test_fetch_autoplay_song_id() {
+		let auto_play_song: String = fetch_autoplay_song_id().await.unwrap();
+		assert!(auto_play_song.eq("048794"));
+	}
+
+	#[tokio::test]
+	async fn test_fetch_song_details() {
+		let song: Song = fetch_song_details("880318").await.unwrap();
+		assert!(song.cid_ref().eq("880318"));
+		assert!(song.name_ref().eq("Protocol"));
+		assert!(song.album_cid_ref().eq("7769"));
+	}
 }
---
+++ src/types/song.rs
@@ -0,0 +1,93 @@
+use getters2::Getters;
+use reqwest;
+use serde;
+use std::{error::Error, result::Result};
+
+#[derive(serde::Deserialize, serde::Serialize, Getters)]
+pub struct SongSyn {
+    cid: String,
+    name: String,
+    #[serde(alias = "albumCid")]
+    album_cid: String,
+    artists: Vec<String>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Getters)]
+pub struct Song {
+    cid: String,
+    name: String,
+    #[serde(alias = "albumCid")]
+    album_cid: String,
+    #[serde(alias = "sourceUrl")]
+    source_url: String,
+    #[serde(alias = "lyricUrl")]
+    lyrics_url: Option<String>,
+    #[serde(alias = "mvUrl")]
+    mv_url: Option<String>,
+    #[serde(alias = "mvCoverUrl")]
+    mv_cover_url: Option<String>,
+    artists: Vec<String>,
+}
+
+impl PartialEq for Song {
+    fn eq(&self, other: &Self) -> bool {
+        self.cid == other.cid
+            && self.name == other.name
+            && self.album_cid == other.album_cid
+            && self.source_url == other.source_url
+            && self.lyrics_url == other.lyrics_url
+            && self.mv_url == other.mv_url
+            && self.mv_cover_url == other.mv_cover_url
+            && self.artists == other.artists
+    }
+}
+
+/**
+ * Fetch the list of songs currently available on MSR.
+ * The list consists of song synapsis's only
+ */
+pub async fn fetch_song_list() -> Result<Vec<SongSyn>, Box<dyn Error>> {
+    Ok(fetch_songs_raw().await?.data.list)
+}
+
+/**
+ * Fetch the current auto-play song (set by MSR) of songs currently available on MSR.
+ * The song is only the song ID
+ */
+pub async fn fetch_autoplay_song_id() -> Result<String, Box<dyn Error>> {
+    Ok(fetch_songs_raw().await?.data.autoplay)
+}
+
+/**
+ * Fetch the details of a song by its ID
+ */
+pub async fn fetch_song_details(id: &str) -> Result<Song, Box<dyn Error>> {
+    let res: MSResponse<Song> = reqwest::get(format!(
+        "https://monster-siren.hypergryph.com/api/song/{id}"
+    ))
+    .await?
+    .json::<MSResponse<Song>>()
+    .await?;
+    Ok(res.data)
+}
+
+#[derive(serde::Deserialize)]
+struct ResponseData {
+    list: Vec<SongSyn>,
+    autoplay: String,
+}
+#[derive(serde::Deserialize)]
+#[allow(unused)]
+struct MSResponse<T> {
+    code: i32,
+    msg: String,
+    data: T,
+}
+async fn fetch_songs_raw() -> Result<MSResponse<ResponseData>, Box<dyn Error>> {
+    let res: MSResponse<ResponseData> =
+        reqwest::get("https://monster-siren.hypergryph.com/api/songs")
+            .await?
+            .json::<MSResponse<ResponseData>>()
+            .await?;
+    Ok(res)
+}
